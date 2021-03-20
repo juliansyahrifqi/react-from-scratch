947c5dc8d37aa31941c79f34fdad236937f8760b
juliansyahrifqi <rifqipratama9@gmail.com>
2021-03-21 00:09:28 +0700

    add search params and use react hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Mili" animal="Cat" breed="Persian" />
-      <Pet name="Dongki" animal="Dog" breed="Havanese" />
-      <Pet name="Maja" animal="Bird" breed="Cockatiel" />
+      <SearchParams />
     </div>
   );
 };
